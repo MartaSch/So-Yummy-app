852eec49fff8ee8d76a9128098bf21af040a056e
MartaSch <martaschab27@gmail.com>
2024-08-17 16:37:37 +0200

    update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
