28512ef4dbe32a54210b67ad8541d84434cd5cea
MartaSch <martaschab27@gmail.com>
2024-08-17 17:22:24 +0200

    update server

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 
 const PORT = Number(process.env.PORT) || 5173;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
