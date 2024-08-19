2df9e3d4a160cf3fbd569968dc1b3a7884bf0f13
MartaSch <martaschab27@gmail.com>
2024-08-19 12:03:31 +0200

    update server

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = Number(process.env.PORT) || 5173;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
